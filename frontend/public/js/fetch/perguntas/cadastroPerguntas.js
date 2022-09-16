4595f0cbaf310e67a849cf1f3d7b1b7a176b215b
Thayssa Apolinário <thayssaoliveira55@gmail.com>
2022-09-16 19:14:07 -0300

    fetch de cadastrar pergunta finalizado

--- frontend/public/js/fetch/perguntas/cadastroPerguntas.js
+++ frontend/public/js/fetch/perguntas/cadastroPerguntas.js
@@ -1,4 +1,17 @@
-const { response } = require("express")
+function openModalPerg() {
+    let modal = document.querySelector('.modal-perguntar')
+    modal.style.display = 'block';
+}
+
+function openModalErro() {
+    let modalErro = document.querySelector('.modal-erro-pergunta')
+    modalErro.style.display = 'block';
+}
+
+function fecharModalErro() {
+    let modalErro = document.querySelector('.modal-erro-pergunta')
+    modalErro.style.display = 'none';
+}
 
 function cadastroPerguntas() {
     const url = "http://localhost:3020/pergunta"
@@ -8,6 +21,7 @@ function cadastroPerguntas() {
     const select = document.getElementById("select-perguntar")
     const categoriaNome = select.value
 
+    try {
     fetch(url, {
         method: "POST",
         headers: {
@@ -21,9 +35,13 @@ function cadastroPerguntas() {
         .then(res => {
             if (res.message) {
                 console.log("pergunta cadastrada com sucesso!")
-
+                openModalPerg()
             } else {
                 console.log("Ocorreu um erro!")
+                openModalErro()
             }
         })
+    } catch(error) {
+        openModalErro()
+    }
 }
